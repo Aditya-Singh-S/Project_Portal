6085e18fa43aff5df93422277ae5bafe55042fb6
Aditya Singh <2389375@cognizant.com>
2025-11-10 23:51:21 +0530

    Partially fine tuned SecurityConfig (authorization)

--- AuthService/src/main/java/com/cts/auth/configuration/SecurityConfig.java
+++ AuthService/src/main/java/com/cts/auth/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,6 +35,11 @@ public class SecurityConfig {
 //	}
 	
 	@Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove "ROLE_" prefix
+    }
+	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(10);
 	}
@@ -49,7 +55,9 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http
-			.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+			.authorizeHttpRequests(request -> request
+					.requestMatchers("/auth/userLists").hasRole("HR")
+					.anyRequest().permitAll())
 			.csrf(customizer -> customizer.disable())
 			.httpBasic(Customizer.withDefaults())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
