60374a4fac1cd88b2b14a1edefb1476258a51667
Aditya Singh <2389375@cognizant.com>
2025-11-13 12:21:31 +0530

    Added ProjectService

---
+++ ProjectService/src/main/java/com/cts/project/controller/ProjectController.java
@@ -0,0 +1,60 @@
+package com.cts.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.project.dto.ProjectDTO;
+import com.cts.project.entity.Project;
+import com.cts.project.service.ProjectService;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+	
+	private ProjectService projectService;
+	
+	@Autowired
+	public ProjectController(ProjectService projectService) {
+		this.projectService = projectService;
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Project> addProject(@RequestBody Project project){
+		Project response = projectService.addProject(project);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/{pid}")
+	public ResponseEntity<Project> viewProject(@PathVariable("pid") int pid){
+		Project response = projectService.viewProject(pid);
+		return ResponseEntity.ok(response);
+	}
+	
+	@PutMapping("/{pid}")
+	public ResponseEntity<Project> updateProject(@PathVariable("pid") int pid,@RequestBody ProjectDTO projectDto){
+		Project response = projectService.updateProject(pid, projectDto);
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping("/{pid}")
+	public ResponseEntity<String> deleteProject(@PathVariable("pid") int pid){
+		String response = projectService.deleteProject(pid);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/viewAll")
+	public ResponseEntity<List<Project>> viewAllProjects(){
+		List<Project> response = projectService.viewAllProject();
+		return ResponseEntity.ok(response);
+	}
+}
---
+++ ProjectService/src/main/java/com/cts/project/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,59 @@
+package com.cts.project.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cts.project.dto.ProjectDTO;
+import com.cts.project.entity.Project;
+import com.cts.project.repository.ProjectRepository;
+import com.cts.project.service.ProjectService;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+
+	private ProjectRepository projectRepo;
+	
+	@Autowired
+	public ProjectServiceImpl(ProjectRepository projectRepo) {
+		this.projectRepo = projectRepo;
+	}
+	
+	@Override
+	public Project addProject(Project project) {
+		return projectRepo.save(project);
+	}
+
+	@Override
+	public Project viewProject(int pid) {
+		return projectRepo.findById(pid).orElseThrow(() -> new RuntimeException("Project Not Found"));
+	}
+
+	@Override
+	public Project updateProject(int pid, ProjectDTO dto) {
+		
+		Project toUpdate = projectRepo.findById(pid).get();
+		
+		toUpdate.setPdescription(dto.getPdescription());
+		toUpdate.setEnddate(dto.getEnddate());
+		toUpdate.setPname(dto.getPname());
+		toUpdate.setSkillrequired(dto.getSkillrequired());
+		toUpdate.setTeamsize(dto.getTeamsize());
+		
+		projectRepo.save(toUpdate);
+		
+		return toUpdate;
+	}
+
+	@Override
+	public String deleteProject(int pid) {
+		projectRepo.deleteById(pid);
+		return "Project Deleted";
+	}
+	
+	public List<Project> viewAllProject(){
+		return projectRepo.findAll();
+	}
+
+}
